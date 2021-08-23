d708e6b7885a8ba68b3fa8aab506411c9d53f78d
MoonDongSik <ehdtlr9376@mailplug.co.kr>
2021-08-24 08:46:48 +0900

    [*] Retrofit 예제2개

--- app/src/main/java/com/example/bard/data/source/remote/RetrofitService.kt
+++ app/src/main/java/com/example/bard/data/source/remote/RetrofitService.kt
@@ -1,8 +1,19 @@
 package com.example.bard.data.source.remote
 
+import kotlinx.coroutines.Deferred
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface RetrofitService {
+    /**
+     * CoroutineCallAdapterFactory 를 사용했을 때 API return type
+     */
     @GET("test/")
-    fun getTest(): String
+    fun coroutineCallAdapterFactory(): Deferred<Response<String>>
+
+    /**
+     * CoroutineCallAdapterFactory 를 사용하지 않고 suspend 를 사용했을 때 API return type
+     */
+    @GET("test/")
+    suspend fun suspendRequest(): Response<String>
 }
